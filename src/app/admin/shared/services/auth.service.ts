857cbf2c422a6b8fed7e83cc36acea6940551ee3
sunprime <sunprime@ukr.net>
2021-03-15 14:56:52 +0100

    auth handle errors

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,33 +1,72 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {User} from '../../../shared/interfaces';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, Subject, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 import {environment} from '../../../../environments/environment';
+import {User} from '../../../shared/interfaces';
 
-const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[${environment.apiKey}]`;
+const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`;
 
 @Injectable()
 export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>();
+
   constructor(private http: HttpClient) {
   }
 
-  get token(): string {
-    return '';
+  get token(): string | null {
+    const currentDate: any = localStorage.getItem('fb-token-exp');
+    const expDate = new Date(currentDate);
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
-  login(user: User): Observable<any>{
-    return this.http.post(url, user);
+  private setToken(response: any): void {
+    console.log('Response', response);
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+  }
+
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true;
+    return this.http.post(url, user).pipe(
+      tap(this.setToken),
+      catchError(this.handleError.bind(this))
+    );
   }
 
   logout(): void {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(): void {
+  private handleError( error: HttpErrorResponse): any {
+    const {message} = error.error.error;
 
+    switch (message) {
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Email is wrong');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Password is wrong');
+        break;
+      case 'USER_DISABLED':
+        this.error$.next('User dont exist');
+        break;
+    }
+
+    return throwError(error);
   }
 }
--- src/environments/interface.ts
+++ src/environments/interface.ts
@@ -2,3 +2,8 @@ export interface Environment {
   production: boolean;
   apiKey: string;
 }
+
+export interface FbAuthResponse {
+  idToken: string;
+  expiresIn: string;
+}
